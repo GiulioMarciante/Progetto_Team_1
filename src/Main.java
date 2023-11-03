41e4fc59aeafa7c252399cb7c212f1d21ff902d0
Erik <mariglianoerik@gmail.com>
2023-11-03 15:25:18 +0100

    Aggiunta funzioneAddizione

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-ajdajdh
+    }
+
+    public static void funzioneAddizione (int num1, int num2) {
+
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+
+    public static void funzioneMoltiplicazione (int num1, int num2) {
+
+        int result = num1 * num2;
+        System.out.println(result);
+    }
+    public static void funzionePariDispari (int num1){
+
+        boolean isPari = false;
+        if (num1 % 2 == 0){
+            isPari = true;
+            System.out.println("Numero Pari");
+        } else {
+            isPari = false;
+            System.out.println("Numero Dispari");
+        }
+        System.out.println(isPari);
     }
 }
