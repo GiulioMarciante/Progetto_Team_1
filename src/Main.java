7c895c610e1fc370aeea1646bd09c1a1a3551369
Giulio Marciante <87944393+undeermind@users.noreply.github.com>
2023-11-03 16:18:04 +0100

    funzione riconoscimento operazione

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,74 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-    }
-    public static void funzioneMoltiplicazione (int num1, int num2) {
+        Scanner scanner = new Scanner(System.in);
 
-        int result = num1 * num2;
-        System.out.println(result);
+        System.out.println("Inserisci il primo numero:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Inserisci l'operatore ( + , - , * , / , ^, p):");
+        char operatore = scanner.next().charAt(0);
+
+        if (operatore != '*' && operatore != 'p'){
+            System.out.println("Inserisci l'operatore corretto ( + , - , * , / , ^, p):");
+            operatore = scanner.next().charAt(0);
+        }
+
+        double num2 = 0;
+
+        if(operatore != 'p'){
+            System.out.println("Inserisci il secondo numero:");
+            num2 = scanner.nextDouble();
+        }
+
+        scanner.close();
+
+        switch(operatore){
+           /*case '+':
+                funzioneAddizione();
+                break;
+            case '-':
+                funzioneSottrazione();
+                break;*/
+            case '*':
+                funzioneMoltiplicazione(num1,num2);
+                break;
+            /*case '/':
+                funzioneDivisione();
+                break;
+            case '^':
+                funzionePotenza();
+                break;*/
+            case 'p':
+                funzionePariDispari(num1);
+                break;
+            default:
+                System.out.println("Not Operator!");
+        }
     }
-    public static void funzionePariDispari (int num1){
+    /*public static double riconoscimentoOperazione(){
+        int[] result = new int [2];
+        char[] operator = new char[1];
+        if (risultato % 2 == 0){
+            funzionePariDispari();
+        }
+    }*/
+
+    public static void funzioneMoltiplicazione (double num1, double num2) {
+
+        double result = num1 * num2;
+        System.out.println(result);
+
+    }
+    public static void funzionePariDispari (double num1){
 
         boolean isPari = false;
         if (num1 % 2 == 0){
             isPari = true;
             System.out.println("Numero Pari");
         } else {
-            isPari = false;
             System.out.println("Numero Dispari");
         }
-        System.out.println(isPari);
     }
 }
