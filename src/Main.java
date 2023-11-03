f2af70b02c149ff07d361c9eca8906adc5c221aa
Erik <mariglianoerik@gmail.com>
2023-11-03 16:37:35 +0100

    Aggiunta funzioneAddizione

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-public class Main {
-    public static void main(String[] args) {
-    }
-
     public static void funzioneAddizione (double num1, double num2) {
 
         double result = num1 + num2;
