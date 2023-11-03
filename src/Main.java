5e7c38b743075a2ee43091086f33c77d55eb762b
LisaFabro <fabro.lisa@gmail.com>
2023-11-03 12:58:30 +0100

    add func potenza con 2 int e stamp esterno

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ciao");
+
+        int num1 = 3;
+        int num2 = 2;
+
+        multi(num1, num2);
+
+        public static void multi(int num, int esp){
+            int resMulti = num;
+            for (int i = 0; i <= esp; i++) {
+                resMulti *= num1;
+                System.out.println(resMulti);
+            }
+        }
     }
 }
